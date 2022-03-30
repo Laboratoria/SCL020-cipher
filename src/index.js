885558a7ead879adef9590500c17fe2152c341eb
Carolina Lobos <loboscarolinaa@gmail.com>
2022-03-30 10:51:09 -0300

    se actualizaron los html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import cipher from './cipher.js';
 
+
 console.log(cipher);
+
+//document.querySelector("cifrado").addEventListener("click", function ()
+
+function pepe(){
+    document.write('<img src="ojos.jpg"/>');
+}
