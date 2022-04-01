a445bcf94a439c92188d764997df85092ce1d97a
AlejandraMogollon <alejandrame1505@gmail.com>
2022-04-01 08:54:01 -0300

    funcion boton code

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,29 @@
 const cipher = {
-  // ...
+  
+  encode,
+  decode
+
+
 };
 
 export default cipher;
+
+
+// Aqui tienes 2 opciones o exportas la funcion de una colocando export funtion encode(){} o 
+// metes dentro del objeto cipher arriba el nombre de la funcion para cuando la tengas que llamar
+// seria ciper.encode()
+function encode(originalMessage,shift){
+
+  let abc= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+
+}
+
+/*export function encode(){
+  console.log('entre a la wea desde cipher');
+}*/
+
+function decode(){
+
+  console.log("mensaage decifrado");
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+
+// console.log(cipher);
+
+
+ window.addEventListener("load", start, true);
+
+function start (){
+  document.getElementById("original").addEventListener(
+    "keyup",
+    function (){
+      this.value = this.value.toUpperCase();
+    },
+    true
+  );
+}
+
+// La wea del type="Module" es complicado pero en internet dice que para crear funciones,variables,etc
+// tienes que usar window.nombrevariable = *lo que quieres hacer(funciones,datos,objetos,arrays)*
+window.eventCode = function eventCode(){
+
+    let originalMessage = document.getElementById("original").value;
+    let shift = document.getElementById("shift").value;
+
+    if (originalMessage === "" || shift === ""){
+        alert ("Debe Llenar campo");
+    };
+
+    let encodeMessage = cipher.encode(originalMessage,shift); 
+
+    document.getElementById("encrypted").value = encodeMessage;
+    
+    
+    // Aqui debes obtener los datos del TextArea y Shift y guardarlos en variables
+    // y despues se lo pasas como argumentos a la funcion encode que esta en el cipher.js
+
+    // cipher.encode()
+    // Aqui adentro llamas la funcion encode(argumento1, argumento2)
+}
+
