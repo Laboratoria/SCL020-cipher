466ad4cf37484d9848fc883d6c8c645964e793f5
Constanza <c.leytonleiva@gmail.com>
2022-04-04 10:35:05 -0400

    cambios

--- src/cipher.js
+++ src/cipher.js
@@ -3,3 +3,90 @@ const cipher = {
 };
 
 export default cipher;
+function encodeCesar() {
+  
+  let original = document.getElementById("original").value;
+  let displace = document.getElementById("displace").value;
+  let newOriginalCode = "";
+  let cipher = "";
+
+  console.log(original);
+  console.log(typeof displace);
+
+  for (let i=0; i < original.length; i++) {
+
+      let originalCode = original[i].charCodeAt();
+      console.log(originalCode);
+      if ((originalCode >= 65) && (originalCode <= 90)) {
+        newOriginalCode = ((originalCode - 65 + parseInt(displace)) % 26 + 65);
+      }
+      else {
+        alert("SOLO LETRAS MAYUSCULAS");
+        break;
+      }
+    cipher += String.fromCharCode(newOriginalCode);
+  }
+    document.getElementById("encrypted").innerHTML = cipher;
+}
+
+
+
+
+function decodeCesar() {
+
+  let original = document.getElementById("original").value;
+  let displace = document.getElementById("displace").value;
+  let newOriginalCode = "";
+  let cipher = "";
+
+  console.log(original);
+  console.log(typeof displace);
+
+  for (let i=0; i < original.length; i++) {
+
+      let originalCode = original[i].charCodeAt();
+      console.log(originalCode);
+      if ((originalCode >= 65) && (originalCode <= 90)) {
+        newOriginalCode = ((originalCode - 65 - parseInt(displace)) % 26 + 65);
+      }
+      else {
+        alert("SOLO LETRAS MAYUSCULAS");
+        break;
+      }
+    cipher += String.fromCharCode(newOriginalCode);
+  }
+    document.getElementById("encrypted").innerHTML = cipher;
+}
+
+
+
+
+
+
+
+
+
+/*
+function cipherCesar(original, displace) {
+  let cipher = "";
+  let newletter = ""; 
+  let i = 0;
+  let original = document.getElementById("original").value;
+  let displace = document.getElementById("displace").value;
+  let newOriginalCode = "";
+
+  for (i=0; i < original.length; i++) {
+      let originalCode = original.charCodeAt(i);
+
+      if (originalCode >= 65 && originalCode <= 90){ 
+        newOriginalCode = (originalCode + displace)%26;
+      }
+    
+      else {
+        alert("nada esta pasando");
+      }
+   }
+        cipher += String.fromCharCode(newOriginalCode);
+  } 
+  document.getElementById("encrypted").innerHTML = cipher;
+}*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+/*REDIRECCION A PAGINAS con window.location*/
+
+function btnFirstPage() {
+    window.location = "/avatar.html";
+  }
+  
+  function btnSecondPage() {
+    window.location = "/firstGame.html";
+  }
+  
+  function btnThirdPage() {
+    window.location = "/secondGame.html";
+  }
+  
+  function btnEndingPage() {
+    window.location = "/endingGame.html";
+  }
+  
+  function btnWelcomePage() {
+    window.location = "/welcome.html";
+  }
+  
+  /* USANDO LOCAL STORAGE*/
+  
+  function getName() {
+  let userName = document.getElementById('user-name').value;
+  localStorage.setItem('userName', userName);
+  
+  let nameLocalStorage = localStorage.getItem('userName');
+  
+  document.getElementById("print-name").innerHTML = "¡Hola " + nameLocalStorage + " vamos a jugar!";
+  }
+  
+  let text= document.getElementById("encrypted");
+  //let button= document.getElementById("btn");
+  btn.addEventListener('click', e => {
+      text.select();
+      document.execCommand('copy');
+    })
