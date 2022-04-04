4c6e7cde71018cfb0583e0e55967b754c77478a6
Constanza <c.leytonleiva@gmail.com>
2022-04-04 16:57:23 -0400

    arreglo de botones addevent

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-import cipher from './cipher.js';
+/*REDIRECCION A PAGINAS con window.location*/
+document.getElementById("welcomebtn").addEventListener("click", function() {window.location = "avatar.html";
+})
+document.getElementById("avatarbtn").addEventListener("click", function() {window.location = "firstGame.html";
+console.log(holi)
+})
+document.getElementById("firstGamebtn").addEventListener("click", function() {window.location = "secondGame.html";
+})
+document.getElementById("SecondGamebtn").addEventListener("click", function() {window.location = "endingGame.html";
+})
+document.getElementById("endingGamebtn").addEventListener("click", function() {window.location = "index.html";
+})
 
-console.log(cipher);
+
+
+/*function btnFirstPage() {
+    window.location = "/avatar.html";
+ }
+function btnSecondPage() {
+    window.location = "/firstGame.html";
+ }  
+function btnThirdPage() {
+    window.location = "/secondGame.html";
+ }  
+function btnEndingPage() {
+    window.location = "/endingGame.html";
+ }   
+function btnWelcomePage() {
+    window.location = "/welcome.html";
+ }/*
+  
+  /* USANDO LOCAL STORAGE*/
+function getName() {
+   let userName = document.getElementById('user-name').value;
+   localStorage.setItem('userName', userName);
+   let nameLocalStorage = localStorage.getItem('userName');
+  document.getElementById("print-name").innerHTML = "¡Hola " + nameLocalStorage + " vamos a jugar!";
+}
+let text= document.getElementById("encrypted");
+btn.addEventListener('click', e => {
+  text.select();
+  document.execCommand('copy');
+  })
