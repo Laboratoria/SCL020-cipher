2894e18822d630e3344669f913b36eca92ecc02d
Constanza <c.leytonleiva@gmail.com>
2022-04-06 15:24:21 -0400

    css endingGame

--- src/index.js
+++ src/index.js
@@ -1,39 +1,19 @@
-import cipher from './cipher.js';
+//import cipher from './cipher.js';
 
-console.log(cipher);
-
-/*REDIRECCION A PAGINAS con window.location*/
-document.getElementById("indexPage").addEventListener("click", ()=>{
-  window.location = "avatar.html"
-})
-  
-let secondPage = document.getElementById("secondPage")
-secondPage.addEventListener("click", ()=>{
-  window.location = "firstGame.html"
-})
-
-
-function btnThirdPage() {
-  window.location = "/secondGame.html";
-}
-  
-function btnEndingPage() {
-  window.location = "/welcome.html";
-  }
-  
+//console.log(cipher);
+document.getElementById("welcomebtn").addEventListener("click", function() {window.location ="avatar.html";
+}) 
   /* USANDO LOCAL STORAGE*/
   
-  function getName() {
+function getName() {
   let userName = document.getElementById('user-name').value;
   localStorage.setItem('userName', userName);
-  
   let nameLocalStorage = localStorage.getItem('userName');
-  
   document.getElementById("print-name").innerHTML = "¡Hola " + nameLocalStorage + " vamos a jugar!";
   }
-  
-  //let text= document.getElementById("encrypted");
-  //btn.addEventListener('click', e => {
-      //text.select();
-      //document.execCommand('copy');
-    //})
+ /*COPIAR*/ 
+let text= document.getElementById("encrypted");
+  btn.addEventListener('click', e => {
+      text.select();
+      document.execCommand('copy');
+    })
