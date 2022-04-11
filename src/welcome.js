a1dff4c7e97b122de05f03ab1ca419efd607baac
fabibbc <fbelenbc@gmail.com>
2022-04-11 09:54:28 -0400

    modificando archivos html y css

--- src/welcome.js
+++ src/welcome.js
@@ -1,9 +1,2 @@
-document.getElementById("welcomebtn").addEventListener("click", function() {window.location = "avatar.html";
-})
-
-document.getElementById("getnamebtn").addEventListener("click", function() {
-    let userName = document.getElementById('user-name').value;
-    localStorage.setItem('userName', userName);
-    let nameLocalStorage = localStorage.getItem('userName');
-document.getElementById("avatarp").innerHTML = "¡Hola " + nameLocalStorage + " vamos a jugar!";
+document.getElementById("welcomebtn").addEventListener("click", function() {window.location = "firstGame.html";
 })
