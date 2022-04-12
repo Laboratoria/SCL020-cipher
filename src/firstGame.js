a4d6ac9abfdbd467622a06b1e2b43fe0c80010e0
fabibbc <fbelenbc@gmail.com>
2022-04-12 08:17:35 -0400

    modificaciones css

--- src/firstGame.js
+++ src/firstGame.js
@@ -1,4 +1,4 @@
-document.getElementById("firstGamebtn").addEventListener("click", function() {window.location ="secondGame.html";
+document.getElementById("welcomebtn").addEventListener("click", function() {window.location ="secondGame.html";
 })
 document.getElementById("btn-encode").addEventListener("click", function() {encodeCesar();
 })
