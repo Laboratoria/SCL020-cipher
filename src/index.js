c59e011273130dbd0fdc5c0f5fdb10d02eac4aa0
caro <loboscarolinaa@gmail.com>
2022-04-18 10:45:06 -0400

    listo para el comiezo con js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import cipher from './cipher.js';
-
-
-console.log(cipher);
-
-//document.querySelector("cifrado").addEventListener("click", function ()
-
-function pepe(){
-    document.write('<img src="ojos.jpg"/>');
+const lenguajes = ["python","ruby","c#","javascript","cobol","assembly","ajax","ruby","javascript","scratch","delphi","j#","perl","lisp","lua","go","haskell","tcl","sas","groovy","r","pascal","prolog",".net","javascript"];
+let totalVeces = 0;
+for (let i = 0; i < lenguajes.length; i++) {
+    console.log(lenguajes[i])
+  if (lenguajes[i] === "javascript") {
+    totalVeces++;
+  }
 }
+alert("La cantidad de veces que aparece javascript es: " + totalVeces)
+
