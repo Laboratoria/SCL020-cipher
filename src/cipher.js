155b1549335c9fbb2d963f6b8d40b25f85116702
caro <loboscarolinaa@gmail.com>
2022-04-26 14:53:12 -0400

    se volvio de 0 se agrego js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,43 @@
 const cipher = {
-  // ...
-};
+        function cifrar(texto, desplazamiento) {
+            let resultado = "";
+            let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-export default cipher;
+            desplazamiento = (desplazamiento % 26 + 26) % 26;
+
+            if (texto) {
+                for (let i = 0; i < texto.length; i++) {
+                    if (letras.indexOf(texto[i]) != -1) {
+                        let posicion = ((letras.indexOf(texto[i]) + desplazamiento) % 26);
+                        resultado += letras[posicion];
+                    } else
+                        resultado += texto[i];
+                }
+
+            }
+            console.log(resultado)
+            return resultado;
+        }
+
+        function descifrar(texto, desplazamiento) {
+            let resultado = "";
+            let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            desplazamiento = (desplazamiento % 26 + 26) % 26;
+
+            if (texto) {
+                for (let i = 0; i < texto.length; i++) {
+                    if (letras.indexOf(texto[i]) != -1) {
+                        let posicion = ((letras.indexOf(texto[i]) - desplazamiento) % 26);
+                        resultado += letras[posicion];
+                    } else
+                        resultado += texto[i];
+                }
+
+            }
+            console.log(resultado)
+            return resultado
+                // ...
+
+        }
+        export default cipher;
